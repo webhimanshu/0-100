4397831b611e1fbc5dc3025bafb5ca2eb6baa9e8
webhimanshu <himanshusharma2709@gmail.com>
2024-03-17 17:47:27 +0530

    db integrated

--- my-next-app/app/(auth)/signin/page.tsx
+++ my-next-app/app/(auth)/signin/page.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 async function getUserDetails() {
   await new Promise((resolve)=>setTimeout(resolve,4000))
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details");
+  // const response = await axios.get("http://localhost:3000/api/user"); this api will point to nextjs api/user/route.ts
 	return response.data;
 }
 
---
+++ my-next-app/app/api/user/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest , NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
+export function GET() {
+    return Response.json({
+        msg: "Hello from Next.js Server"
+    });
+}
+//localhost:3000/api/user
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
+}

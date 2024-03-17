c9ece970e82b7d6498263c9d1dc1ef0f1a3dd04e
webhimanshu <himanshusharma2709@gmail.com>
2024-03-17 16:33:36 +0530

    ssr logic added

--- my-next-app/app/(auth)/signin/page.tsx
+++ my-next-app/app/(auth)/signin/page.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import axios from "axios";
 
-export default function Home(){
+async function getUserDetails() {
+  await new Promise((resolve)=>setTimeout(resolve,4000))
+  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+	return response.data;
+}
+
+export default async function Home() {
+  
+  const userData = await getUserDetails();
+
   return (
-    <div>sign in page</div>
-  )
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userData?.name}
+                </div>
+                
+                {userData?.email}
+            </div>
+        </div>
+    </div>
+  );
 }
---
+++ my-next-app/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div className="text-red-900 font-medium text-3xl">
+            Loading ..............
+        </div>
+    )
+}

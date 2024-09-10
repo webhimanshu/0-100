45f83d416b136471c532e4e8a77593e09d9f4d5d
webhimanshu <himanshusharma2709@gmail.com>
2024-09-10 20:29:44 +0530

    dummy webhook added for hdfc

--- paytm-clone/apps/web/app/page.tsx
+++ paytm-clone/apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div className="">
       <main className={styles.main}>
         <Image
           className={styles.logo}
